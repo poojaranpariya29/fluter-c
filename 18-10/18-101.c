4e86a3e011d7b12450b25c1d754f9f8ca5e3fe3d
poojaranpariya29 <148708401+poojaranpariya29@users.noreply.github.com>
2023-12-04 23:28:22 +0530

    Add files via upload

---
+++ 18-10/18-101.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+int main(){ 
+    int a=12;
+       int b=6;
+    
+printf("Addition of %d and %d is %d\n",a=12,b=6,a+b);
+printf("subtraction of %d and %d is %d\n",a=12,b=6,a-b);
+printf("Multiplication of %d and %d is %d\n",a=12,b=6,a*b);
+printf("Division of %d and %d is %d\n",a=12,b=6,a/b);
+printf("Modulo of %d and %d is %d",a=12,b=6,a%b);
+return 0;
+}
---
+++ 18-10/18-102.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a = 12;
+    int b = 6;
+    printf("%d + %d = %d\n", a, b, a + b);
+    printf("%d - %d = %d\n", a, b, a - b);
+    printf("%d * %d = %d\n", a, b, a * b);
+    printf("%d / %d = %d\n", a, b, a / b);
+    printf("%d % %d = %d\n", a, b, a % b);
+    return 0;
+}
